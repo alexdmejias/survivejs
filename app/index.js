f32d9ab680876512db2d143e380e260be1a5360b
Alex Mejias <alexdmejias@gmail.com>
2016-05-18 00:33:28 -0400

    completed ch2

---
+++ app/index.js
@@ -0,0 +1,9 @@
+var component = require('./component.js');
+
+require('./main.css');
+
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
+  }
+}
+
+// default configuration
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devTool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {});
+}
